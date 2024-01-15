3ca5630c4c433dd6911039ad83fd9525f272e865
sw212 <sw212@github.com>
2024-01-15 14:04:41 +0000

    fix task3 error & erroneous comma in endpoints.json

--- models/api.models.js
+++ models/api.models.js
@@ -1,5 +1,7 @@
 const fs = require("fs/promises");
 
-module.exports.fetchAPI = () => {
-    return fs.readFile("endpoints.json", "utf-8");
+module.exports.fetchAPI = async () => {
+    const endpoints = await fs.readFile("endpoints.json", "utf-8");
+    const result = JSON.parse(endpoints);
+    return result;
 }
